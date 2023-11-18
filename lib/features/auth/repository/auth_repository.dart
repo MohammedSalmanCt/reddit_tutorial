b4f565c8adc6b207c8d9e4b246aa5737239c4a52
SALMAN <ctsalmanct@gmail.com>
2023-11-18 23:39:28 +0530

    aa

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -9,71 +9,68 @@ import 'package:reddit_tutorial/core/providers/firebase_provider.dart';
 import 'package:reddit_tutorial/models/add_user-model.dart';
 import '../../../core/type_def.dart';
 
-
-
 final authRepositoryProvider = Provider<AuthRepository>((ref) {
-  return AuthRepository(firestore: ref.read(firestoreProvider),
+  return AuthRepository(
+      firestore: ref.read(firestoreProvider),
       auth: ref.read(authProvider),
       googleSignIn: ref.read(googleSignInProvider));
 });
 
-class AuthRepository{
+
+class AuthRepository {
   final FirebaseFirestore _firestore;
   final FirebaseAuth _auth;
   final GoogleSignIn _googleSignIn;
 
-  AuthRepository({
-    required FirebaseFirestore firestore,
-    required FirebaseAuth auth,
-    required GoogleSignIn googleSignIn
-}):_firestore =firestore,
-  _auth=auth,
-  _googleSignIn=googleSignIn;
+  AuthRepository(
+      {required FirebaseFirestore firestore,
+      required FirebaseAuth auth,
+      required GoogleSignIn googleSignIn})
+      : _firestore = firestore,
+        _auth = auth,
+        _googleSignIn = googleSignIn;
 
-  CollectionReference get _users=>_firestore.collection(FirebaseConstants.usersCollection);
+  CollectionReference get _users =>
+      _firestore.collection(FirebaseConstants.usersCollection);
 
-  Stream<User?> get authStateChange=>_auth.authStateChanges();
+  Stream<User?> get authStateChange => _auth.authStateChanges();
 
- FutureEither<UserModel> signWithGoogle()
-  async{
-    try{
+  FutureEither<UserModel> signWithGoogle() async {
+    try {
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-      final googleAuth=await googleUser?.authentication;
-      final credential= GoogleAuthProvider.credential(
-        accessToken: googleAuth?.accessToken,
-        idToken: googleAuth?.idToken
-      );
+      final googleAuth = await googleUser?.authentication;
+      final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth?.accessToken, idToken: googleAuth?.idToken);
 
-      UserCredential userCredential=await _auth.signInWithCredential(credential);
+      UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
       UserModel userModel;
-     if(userCredential.additionalUserInfo!.isNewUser)
-       {
-         userModel=UserModel(name: userCredential.user!.displayName,
-             age: null,
-             phone: null,
-             email: userCredential.user!.email,
-             id: userCredential.user!.uid);
+      if (userCredential.additionalUserInfo!.isNewUser) {
+        userModel = UserModel(
+            name: userCredential.user!.displayName ?? "Untitled",
+            profilePic: userCredential.user!.photoURL ?? "",
+            banner: "",
+            uid: userCredential.user!.uid,
+            isAuthenticated: true,
+            karma: 0,
+            awards: []);
 
-        await _users.doc(userCredential.user!.uid).set(userModel.toJson());
-       }
-     else
-       {
-         userModel=await getUserData(userCredential.user!.uid).first;
-       }
-     return right(userModel);
-    }
-   on FirebaseException catch(e)
-    {
+        await _users.doc(userCredential.user!.uid).set(userModel.toMap());
+      } else {
+        userModel = await getUserData(userCredential.user!.uid).first;
+      }
+      return right(userModel);
+    } on FirebaseException catch (e) {
       throw e.message!;
-    }
-    catch (e)
-    {
+    } catch (e) {
       return left(Failure(message: e.toString()));
     }
   }
 
-  Stream<UserModel> getUserData(String id)
-  {
-   return _users.doc(id).snapshots().map((event) => UserModel.fromJson(event.data() as Map<String,dynamic>));
+  Stream<UserModel> getUserData(String id) {
+    return _users.doc(id).snapshots().map(
+        (event){
+        return  UserModel.fromMap(event.data() as Map<String, dynamic>);
+        });
   }
 }
