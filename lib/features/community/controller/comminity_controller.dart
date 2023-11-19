db40b2de6b77c0aa96eb608d6cb8971c222ca42b
SALMAN <ctsalmanct@gmail.com>
2023-11-19 12:59:42 +0530

    19

---
+++ lib/features/community/controller/comminity_controller.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_tutorial/features/auth/controller/auth_controller.dart';
+import 'package:reddit_tutorial/features/community/repository/community_reository.dart';
+import 'package:reddit_tutorial/models/community_model.dart';
+import 'package:routemaster/routemaster.dart';
+import '../../../core/utils.dart';
+
+final communityControllerProvider =
+    StateNotifierProvider<CommunityController, bool>((ref) {
+  return CommunityController(
+      communityRepository: ref.watch(communityRepositoryProvider), ref: ref);
+});
+
+class CommunityController extends StateNotifier<bool> {
+  final CommunityRepository _communityRepository;
+  final Ref _ref;
+  CommunityController(
+      {required CommunityRepository communityRepository, required Ref ref})
+      : _communityRepository = communityRepository,
+        _ref = ref,super(false);
+
+  void createCommunity(String name, BuildContext context) async {
+    state=true;
+    final uid = _ref.read(userProvider)?.name??"";
+    Community community = Community(
+        id: name,
+        name: name,
+        banner: "",
+        avatar: "",
+        members: [uid],
+        mods: [uid]);
+   final res=await _communityRepository.createCommunity(community);
+   state=false;
+   res.fold((l) => showSnackBar(context,l.message), (r){
+     Routemaster.of(context).pop();
+   });
+  }
+}
--- lib/features/community/screens/create_community_screen.dart
+++ lib/features/community/screens/create_community_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_tutorial/core/common/loader.dart';
+
+import '../controller/comminity_controller.dart';
 
 class CreateCommunityScreen extends ConsumerStatefulWidget {
   const CreateCommunityScreen({super.key});
@@ -9,17 +12,67 @@ class CreateCommunityScreen extends ConsumerStatefulWidget {
 }
 
 class _CreateCommunityScreenState extends ConsumerState<CreateCommunityScreen> {
+  final communityNameController = TextEditingController();
+  void createCommunity() {
+    ref
+        .read(communityControllerProvider.notifier)
+        .createCommunity(communityNameController.text.trim(), context);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    communityNameController.dispose();
+  }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
+    final isLoading=ref.watch(communityControllerProvider);
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.black,
       appBar: AppBar(
         title: Text("Create a community"),
+      ),
+      body: isLoading?Loader():Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            const Align(
+                alignment: Alignment.topLeft, child: Text("Community Name")),
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: communityNameController,
+              decoration: const InputDecoration(
+                  hintText: "Community name",
+                  filled: true,
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.all(18)),
+              maxLength: 21,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                createCommunity();
+              },
+              style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20))),
+              child: const Text("Create community"),
+            )
+          ],
+        ),
       ),
     );
   }
