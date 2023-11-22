cda1649e1ece47e0b6f3c0a6a3dde8b6260fcae6
SALMAN <ctsalmanct@gmail.com>
2023-11-22 21:59:08 +0530

    20

--- lib/core/providers/storage_repositiry_provider.dart
+++ lib/core/providers/storage_repositiry_provider.dart
@@ -1,15 +1,24 @@
-import 'dart:ffi';
+import 'dart:io';
 
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:reddit_tutorial/core/failure.dart';
 import 'package:reddit_tutorial/core/type_def.dart';
 
-class StorageRepository{
-  final FirebaseStoragea _firebaseStoragea;
-  StorageRepository({required FirebaseStorage firebaseStorage}):_firebaseStoragea=firebaseStorage;
+class StorageRepository {
+  final FirebaseStorage _firebaseStorage;
+  StorageRepository({required FirebaseStorage firebaseStorage})
+      : _firebaseStorage = firebaseStorage;
 
-FutureEither<String?> storeFile(requirede String path,required String Id)
-async{
-  _firebaseStoragea.ref().chi;
-}
-
+  FutureEither<String> storeFile(
+      {required String path, required String id, required File? file}) async {
+    try {
+      final ref = _firebaseStorage.ref().child(path).child(id);
+      UploadTask uploadTask = ref.putFile(file!);
+      final snapshot = await uploadTask;
+      return right(await snapshot.ref.getDownloadURL());
+    } catch (e) {
+      return left(Failure(message: e.toString()));
+    }
+  }
 }
