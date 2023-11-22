96d907168d2aabb5d34f3e5f1bec2e0c6cc3b180
SALMAN <ctsalmanct@gmail.com>
2023-11-22 23:47:42 +0530

    20

--- lib/features/community/repository/community_reository.dart
+++ lib/features/community/repository/community_reository.dart
@@ -10,64 +10,75 @@ import 'package:reddit_tutorial/models/community_model.dart';
 final communityRepositoryProvider = Provider<CommunityRepository>((ref) {
   return CommunityRepository(firestore: ref.watch(firestoreProvider));
 });
-class CommunityRepository{
+
+class CommunityRepository {
   final FirebaseFirestore _firestore;
-  CommunityRepository({
-    required FirebaseFirestore firestore
-}):_firestore=firestore;
+  CommunityRepository({required FirebaseFirestore firestore})
+      : _firestore = firestore;
 
+  CollectionReference get _communities =>
+      _firestore.collection(FirebaseConstants.communitiesCollection);
+  FutureVoid createCommunity(Community community) async {
+    try {
+      var communityDoc = await _communities.doc(community.name).get();
+      if (communityDoc.exists) {
+        throw "Community with the same name already exists!";
+      }
 
-  CollectionReference get _communities=> _firestore.collection(FirebaseConstants.communitiesCollection);
-  FutureVoid createCommunity(Community community)
-  async{
-    try{
-      var communityDoc=await _communities.doc(community.name).get();
-      if(communityDoc.exists)
-        {
-          throw "Community with the same name already exists!";
-        }
-
-   return right(await _communities.doc(community.name).set(community.toMap()));
-    }
-    on FirebaseException catch(e)
-    {
-throw e.message!;
-    }
-    catch(error)
-    {
+      return right(
+          await _communities.doc(community.name).set(community.toMap()));
+    } on FirebaseException catch (e) {
+      throw e.message!;
+    } catch (error) {
       return left(Failure(message: error.toString()));
     }
   }
 
-  Stream<List<Community>> getUserCommunities(String uid)
-  {
-    return _communities.where("members",arrayContains: uid).snapshots().map((event) {
-     List<Community> communities=[];
-     for(var i in event.docs)
-       {
-         communities.add(Community.fromMap(i.data() as Map<String,dynamic>));
-       }
-     return communities;
+  Stream<List<Community>> getUserCommunities(String uid) {
+    return _communities
+        .where("members", arrayContains: uid)
+        .snapshots()
+        .map((event) {
+      List<Community> communities = [];
+      for (var i in event.docs) {
+        communities.add(Community.fromMap(i.data() as Map<String, dynamic>));
+      }
+      return communities;
     });
   }
 
   Stream<Community> getCommunityByName(String name) {
     return _communities.doc(name).snapshots().map(
-            (event) => Community.fromMap(event.data() as Map<String, dynamic>));
+        (event) => Community.fromMap(event.data() as Map<String, dynamic>));
   }
-FutureVoid editCommunity(Community community)
-async{
-  try{
-  return right(_communities.doc(community.name).update(community.toMap()));
-  }
-  on FirebaseException catch(e)
-  {
-    throw e.message!;
-  }
-  catch(e)
-  {
-    return left(Failure(message: e.toString()));
-  }
-}
 
+  FutureVoid editCommunity(Community community) async {
+    try {
+      return right(_communities.doc(community.name).update(community.toMap()));
+    } on FirebaseException catch (e) {
+      throw e.message!;
+    } catch (e) {
+      return left(Failure(message: e.toString()));
+    }
+  }
+
+  Stream<List<Community>> searchCommunity(String query) {
+    return _communities
+        .where("name",
+            isGreaterThanOrEqualTo: query.isEmpty ? 0 : query,
+            isLessThan: query.isEmpty
+                ? null
+                : query.substring(0, query.length - 1) +
+                    String.fromCharCode(query.codeUnitAt(query.length - 1) -
+                    1),
+    )
+        .snapshots()
+        .map((event) {
+      List<Community> communities = [];
+      for (var i in event.docs) {
+        communities.add(Community.fromMap(i.data() as Map<String, dynamic>));
+      }
+      return communities;
+    });
+  }
 }
