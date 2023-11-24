39f0ca66cacc7b64d881ba863fb5597afd20da08
SALMAN <ctsalmanct@gmail.com>
2023-11-24 21:08:26 +0530

    24

--- lib/features/home/drawer/profile_drawer.dart
+++ lib/features/home/drawer/profile_drawer.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_tutorial/core/theme/pallet.dart';
 import 'package:reddit_tutorial/features/auth/controller/auth_controller.dart';
 
 class ProfileDrawer extends ConsumerWidget {
   const ProfileDrawer({super.key});
 
+  void logOut(WidgetRef ref)
+  {
+    ref.read(authControlleProvider.notifier).logOut();
+  }
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final user=ref.watch(userProvider);
@@ -15,7 +20,36 @@ class ProfileDrawer extends ConsumerWidget {
             CircleAvatar(
               backgroundImage: NetworkImage(user!.profilePic),
               radius: 70,
-            )
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text("${user.name}",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500
+            ),),
+            SizedBox(
+              height: 10,
+            ),
+            Divider(),
+            ListTile(
+              title: Text("My profile"),
+               leading: Icon((Icons.person),),
+              onTap: () {
+              },
+            ),ListTile(
+              title: Text("Log out"),
+               leading: Icon((Icons.logout),
+                   color: Pallete.redColor,),
+              onTap: () {
+                logOut(ref);
+              },
+            ),
+            Switch.adaptive(value: true,
+              onChanged: (value) {
+
+            },)
           ],
         ),
       ),
