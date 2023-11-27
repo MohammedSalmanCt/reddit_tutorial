21a91d72ba68c81b52e547624c254175627d75e1
SALMAN <ctsalmanct@gmail.com>
2023-11-28 00:01:02 +0530

    27

---
+++ lib/core/enums/enums.dart
@@ -0,0 +1,4 @@
+enum ThemeMode{
+  ligth,
+  dark
+}
--- lib/user_profile/screen/edit_profile.dart
+++ lib/user_profile/screen/edit_profile.dart
@@ -13,7 +13,7 @@ import '../../core/theme/pallet.dart';
 import '../../core/utils.dart';
 
 class EditProfileScreen extends ConsumerStatefulWidget {
-  const EditProfileScreen({super.key,required this.uid});
+  const EditProfileScreen({super.key, required this.uid});
   final String uid;
 
   @override
@@ -24,123 +24,137 @@ class _EditProfileScreenState extends ConsumerState<EditProfileScreen> {
   File? bannerFile;
   File? profileFile;
   late TextEditingController nameController;
-  void selectBannerImage()
-  async{
-    final res=await filePicker();
-    if(res!=null)
-    {
+  void selectBannerImage() async {
+    final res = await filePicker();
+    if (res != null) {
       setState(() {
-        bannerFile=File(res.files.first.path!);
-
-      });
-    }
-  }
-  void selectProfileImage()
-  async{
-    final res=await filePicker();
-    if(res!=null)
-    {
-      setState(() {
-        profileFile=File(res.files.first.path!);
-
+        bannerFile = File(res.files.first.path!);
       });
     }
   }
 
-  void save()
-  {
-    ref.read(userProfileControllerProvider.notifier).editProfile(context: context, profileFile: profileFile, bannerFile: bannerFile, name: nameController.text.trim());
+  void selectProfileImage() async {
+    final res = await filePicker();
+    if (res != null) {
+      setState(() {
+        profileFile = File(res.files.first.path!);
+      });
+    }
   }
 
-@override
+  void save() {
+    ref.read(userProfileControllerProvider.notifier).editProfile(
+        context: context,
+        profileFile: profileFile,
+        bannerFile: bannerFile,
+        name: nameController.text.trim());
+  }
+
+  @override
   void initState() {
     // TODO: implement initState
-  nameController=TextEditingController(text: ref.read(userProvider)!.name);
+    nameController = TextEditingController(text: ref.read(userProvider)!.name);
   }
+
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
     nameController.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref.watch(userProfileControllerProvider);
+    final currentheme=ref.watch(themeNotifierProvider);
     return ref.watch(getUserDataProvider(widget.uid)).when(
       data: (data) {
         return Scaffold(
-          backgroundColor: Pallete.darkModeAppTheme.backgroundColor,
+          backgroundColor:currentheme.colorScheme.background,
           appBar: AppBar(
             title: const Text("Edit Profile"),
             centerTitle: false,
             actions: [
-              TextButton(onPressed: () {
-                save();
-              }, child: const Text("Save"))],
+              TextButton(
+                  onPressed: () {
+                    print("ssssssssssssssssssssssss");
+                    save();
+                    print("object");
+                  },
+                  child: const Text("Save"))
+            ],
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 200,
-                  child: Stack(
-                      children: [
-                        GestureDetector(
-                          onTap: () => selectBannerImage(),
-                          child: DottedBorder(
-                            borderType: BorderType.RRect,
-                            radius: const Radius.circular(10),
-                            dashPattern: const [10, 4],
-                            strokeCap: StrokeCap.round,
-                            color: Pallete.darkModeAppTheme.textTheme.bodyText2!.color!,
-                            child: Container(
-                              width: double.infinity,
-                              height: 150,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10)),
-                              child: bannerFile!=null ?Image.file(bannerFile!) :data.banner.isEmpty ||
-                                  data.banner == Constants.bannerDefault
-                                  ? const Center(
-                                child:
-                                Icon((Icons.camera_alt_outlined), size: 40),
-                              )
-                                  : Image.network(data.banner),
+          body: isLoading
+              ? const Loader()
+              : Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 200,
+                        child: Stack(children: [
+                          GestureDetector(
+                            onTap: () => selectBannerImage(),
+                            child: DottedBorder(
+                              borderType: BorderType.RRect,
+                              radius: const Radius.circular(10),
+                              dashPattern: const [10, 4],
+                              strokeCap: StrokeCap.round,
+                              color: Pallete
+                                  .darkModeAppTheme.textTheme.bodyText2!.color!,
+                              child: Container(
+                                width: double.infinity,
+                                height: 150,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: bannerFile != null
+                                    ? Image.file(bannerFile!)
+                                    : data.banner.isEmpty ||
+                                            data.banner ==
+                                                Constants.bannerDefault
+                                        ? const Center(
+                                            child: Icon(
+                                                (Icons.camera_alt_outlined),
+                                                size: 40),
+                                          )
+                                        : Image.network(data.banner),
+                              ),
                             ),
                           ),
+                          Positioned(
+                            bottom: 20,
+                            left: 20,
+                            child: GestureDetector(
+                              onTap: () => selectProfileImage(),
+                              child: profileFile != null
+                                  ? CircleAvatar(
+                                      backgroundImage: FileImage(profileFile!),
+                                      radius: 32,
+                                    )
+                                  : CircleAvatar(
+                                      backgroundImage:
+                                          NetworkImage(data.profilePic),
+                                      radius: 32,
+                                    ),
+                            ),
+                          )
+                        ]),
+                      ),
+                      TextField(
+                        controller: nameController,
+                        decoration: InputDecoration(
+                          filled: true,
+                          hintText: "Name",
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.blue),
+                              borderRadius: BorderRadius.circular(10)),
+                          border: InputBorder.none,
+                          contentPadding: const EdgeInsets.all(18),
                         ),
-                        Positioned(
-                          bottom: 20,
-                          left: 20,
-                          child: GestureDetector(
-                            onTap: () => selectProfileImage(),
-                            child: profileFile!=null
-                                ?CircleAvatar(
-                              backgroundImage: FileImage(profileFile!),
-                              radius: 32,
-                            )
-                                :CircleAvatar(
-                              backgroundImage: NetworkImage(data.profilePic),
-                              radius: 32,
-                            ),
-                          ),
-                        )
-                      ]),
-                ),
-                TextField(
-                  controller: nameController,
-                  decoration: InputDecoration(
-                    filled: true,
-                    hintText: "Name",
-                    focusedBorder: OutlineInputBorder(
-                       borderSide:BorderSide(color: Colors.blue ),
-                      borderRadius: BorderRadius.circular(10)
-                    ),
-                    border: InputBorder.none,contentPadding: const EdgeInsets.all(18),
+                      )
+                    ],
                   ),
-                )
-              ],
-            ),
-          ),
+                ),
         );
       },
       error: (error, stackTrace) {
@@ -150,5 +164,5 @@ class _EditProfileScreenState extends ConsumerState<EditProfileScreen> {
         return Loader();
       },
     );
-}
+  }
 }
