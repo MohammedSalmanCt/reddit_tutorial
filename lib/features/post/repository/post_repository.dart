5f6c208882861b6f6065cdd8123da10f1a93939d
SALMAN <ctsalmanct@gmail.com>
2023-11-29 23:21:14 +0530

    29

--- lib/features/post/repository/post_repository.dart
+++ lib/features/post/repository/post_repository.dart
@@ -3,33 +3,41 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:reddit_tutorial/core/contants/firebase_constants.dart';
 import 'package:reddit_tutorial/core/providers/firebase_provider.dart';
-
 import '../../../core/failure.dart';
 import '../../../core/type_def.dart';
+import '../../../models/community_model.dart';
 import '../../../models/post_model.dart';
 
 final postRepositoryProvider = Provider((ref) {
   return PostRepository(firestore: ref.watch(firestoreProvider));
 });
 
-class PostRepository{
+class PostRepository {
   final FirebaseFirestore _firestore;
-  PostRepository({required FirebaseFirestore firestore}):_firestore=firestore;
+  PostRepository({required FirebaseFirestore firestore})
+      : _firestore = firestore;
 
-CollectionReference get _posts=>_firestore.collection(FirebaseConstants.postsCollection);
+  CollectionReference get _posts =>
+      _firestore.collection(FirebaseConstants.postsCollection);
 
-  FutureVoid addPost(Post post)
-  async{
+  FutureVoid addPost(Post post) async {
     try {
       return right(_posts.doc(post.id).set(post.toMap()));
-    }
-    on FirebaseException catch(e)
-    {
+    } on FirebaseException catch (e) {
       throw e.message!;
-    }
-    catch(e)
-    {
+    } catch (e) {
       return left(Failure(message: e.toString()));
     }
+  }
+
+  Stream<List<Post>> fetchUserPosts(List<Community> communities) {
+    return _posts
+        .where("communityName",
+            whereIn: communities.map((e) => e.name).toList())
+        .orderBy("createdAt", descending: true)
+        .snapshots()
+        .map((event) => event.docs
+            .map((e) => Post.fromMap(e.data() as Map<String, dynamic>))
+            .toList());
   }
 }
