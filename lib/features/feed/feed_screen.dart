3b3d0bc60e25af1a07ba13523b62b90072383e8d
SALMAN <ctsalmanct@gmail.com>
2023-11-29 23:21:49 +0530

    29

--- lib/features/feed/feed_screen.dart
+++ lib/features/feed/feed_screen.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_tutorial/core/common/loader.dart';
+import 'package:reddit_tutorial/core/common/post_card.dart';
+import 'package:reddit_tutorial/features/community/controller/comminity_controller.dart';
+import 'package:reddit_tutorial/features/post/controller/post_controller.dart';
 
 class FeedScreen extends ConsumerWidget {
   const FeedScreen({super.key});
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-
-    );
+    return ref.watch(userCommunitiesProvider).when(data: (communities) {
+      return ref.watch(userPostProvider(communities)).when(data: (data) {
+return ListView.builder(
+  itemCount:data.length ,
+  itemBuilder: (context, index) {
+final post=data[index];
+return PostCard(post: post);
+},);
+      }, error: (error, stackTrace) {
+        return Text(error.toString());
+      }, loading: () {
+        return Loader();
+      },);
+    }, error: (error, stackTrace) {
+      return Text(error.toString());
+    }, loading: () {
+      return Loader();
+    },);
   }
 }
