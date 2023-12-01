6d5fdd3e34a4042ca59a3419c04793b61f87e6cc
SALMAN <ctsalmanct@gmail.com>
2023-12-01 23:31:28 +0530

    1

---
+++ lib/features/post/screen/widget/comment_card.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_tutorial/models/comment_model.dart';
+
+class CommentCard extends ConsumerWidget {
+  const CommentCard({super.key,required this.comment});
+  final CommentModel comment;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 4,vertical: 10),
+      child: Column(
+        crossAxisAlignment:  CrossAxisAlignment.start,
+mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(comment.profilePic,),
+                radius: 18,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(comment.username,style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      )),
+                      Text(comment.text),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              IconButton(onPressed: () {
+
+              }, icon: const Icon(Icons.reply)),
+              const Text("reply")
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
