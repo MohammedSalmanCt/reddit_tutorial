7ba4fc109eae50433d1b5f61fc69c1c9f593dad5
SALMAN <ctsalmanct@gmail.com>
2023-12-03 19:49:43 +0530

    3

--- lib/user_profile/controller/user_profile_controller.dart
+++ lib/user_profile/controller/user_profile_controller.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_tutorial/core/enums/enums.dart';
 import 'package:reddit_tutorial/core/providers/storage_repositiry_provider.dart';
 import 'package:reddit_tutorial/features/auth/controller/auth_controller.dart';
 import 'package:reddit_tutorial/models/add_user-model.dart';
@@ -12,58 +13,69 @@ import '../../core/utils.dart';
 import '../../models/post_model.dart';
 
 final userProfileControllerProvider =
-StateNotifierProvider<UserProfileController, bool>((ref) {
+    StateNotifierProvider<UserProfileController, bool>((ref) {
   return UserProfileController(
       userProfileRepoitory: ref.watch(userProfileRepositoryProvider),
       ref: ref,
       storageRepository: ref.watch(storageRepositoryProvider));
 });
 
-final getUserPostsProvider = StreamProvider.family((ref, String uid)  {
+final getUserPostsProvider = StreamProvider.family((ref, String uid) {
   return ref.read(userProfileControllerProvider.notifier).getPost(uid);
 });
 
-class UserProfileController extends StateNotifier<bool>{
+class UserProfileController extends StateNotifier<bool> {
   final UserProfileRepoitory _profileRepoitory;
   final Ref _ref;
   final StorageRepository _storageRepository;
-  UserProfileController({required UserProfileRepoitory userProfileRepoitory,required Ref ref,required StorageRepository storageRepository}):_profileRepoitory=userProfileRepoitory,
-  _ref=ref,
-  _storageRepository=storageRepository,
-  super(false);
+  UserProfileController(
+      {required UserProfileRepoitory userProfileRepoitory,
+      required Ref ref,
+      required StorageRepository storageRepository})
+      : _profileRepoitory = userProfileRepoitory,
+        _ref = ref,
+        _storageRepository = storageRepository,
+        super(false);
 
   Future<void> editProfile(
       {required BuildContext context,
-        required File? profileFile,
-        required File? bannerFile,
+      required File? profileFile,
+      required File? bannerFile,
       required String name}) async {
     state = true;
-    UserModel user=_ref.read(userProvider)!;
+    UserModel user = _ref.read(userProvider)!;
     if (profileFile != null) {
       final res = await _storageRepository.storeFile(
           path: "users/profile", id: user.uid, file: profileFile);
       res.fold((l) => showSnackBar(context, l.message),
-              (r) => user = user.copyWith(profilePic: r));
+          (r) => user = user.copyWith(profilePic: r));
     }
 
     if (bannerFile != null) {
       final res = await _storageRepository.storeFile(
           path: "users/banner", id: user.uid, file: bannerFile);
       res.fold((l) => showSnackBar(context, l.message),
-              (r) => user = user.copyWith(banner: r));
+          (r) => user = user.copyWith(banner: r));
     }
-    user=user.copyWith(name: name);
+    user = user.copyWith(name: name);
     final res = await _profileRepoitory.editProfile(user);
-    res.fold((l) => showSnackBar(context, l.message),
-            (r) {
+    res.fold((l) => showSnackBar(context, l.message), (r) {
       _ref.read(userProvider.notifier).update((state) => user);
-              Routemaster.of(context).pop();
-            });
+      Routemaster.of(context).pop();
+    });
     state = false;
   }
 
-  Stream<List<Post>> getPost(String uid)
-  {
+  Stream<List<Post>> getPost(String uid) {
     return _profileRepoitory.getPost(uid);
+  }
+
+  Future<void> updateUserKarma(
+      UserKarma userKarma, BuildContext context) async {
+    UserModel user = _ref.read(userProvider)!;
+    user = user.copyWith(karma: userKarma.karma);
+    final res = await _profileRepoitory.updateUserKarma(user);
+    res.fold((l) => showSnackBar(context, l.message),
+        (r) => _ref.read(userProvider.notifier).update((state) => user));
   }
 }
