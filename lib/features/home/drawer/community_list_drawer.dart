a7545037a115a781b12377bbf28732412911565a
SALMAN <ctsalmanct@gmail.com>
2023-12-04 00:25:22 +0530

    3

--- lib/features/home/drawer/community_list_drawer.dart
+++ lib/features/home/drawer/community_list_drawer.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_tutorial/core/common/loader.dart';
+import 'package:reddit_tutorial/core/common/sign_in_button.dart';
 import 'package:reddit_tutorial/features/community/controller/comminity_controller.dart';
 import 'package:reddit_tutorial/models/community_model.dart';
 import 'package:routemaster/routemaster.dart';
 
+import '../../auth/controller/auth_controller.dart';
+
 class CommunityListDrawer extends ConsumerWidget {
   const CommunityListDrawer({super.key});
 
-  void navigateToCreateCommunity(BuildContext context)
-  {
-   Routemaster.of(context).push('/create-community');
+  void navigateToCreateCommunity(BuildContext context) {
+    Routemaster.of(context).push('/create-community');
   }
 
   void navigateToCommunity(BuildContext context, Community community) {
@@ -19,36 +21,49 @@ class CommunityListDrawer extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(userProvider)!;
+    final isGuest = !user.isAuthenticated;
     return Drawer(
       child: SafeArea(
         child: Column(
           children: [
-            ListTile(
-              title: const Text("Create a community"),
-              leading: Icon(Icons.add),
-              onTap: ()=>navigateToCreateCommunity(context),
-            ),
-            ref.watch(userCommunitiesProvider).when(data: (data) {
-              return Expanded(
-                child: ListView.builder(itemCount:data.length,
-                itemBuilder: (context, index) {
-                  final community=data[index];
-                  return  ListTile(
-                    title: Text(community.name),
-                    leading:  CircleAvatar(
-                      backgroundImage: NetworkImage(community.avatar),
+            isGuest
+                ? const SignInButton(
+                    isFromLogin: false,
+                  )
+                : ListTile(
+                    title: const Text("Create a community"),
+                    leading: Icon(Icons.add),
+                    onTap: () => navigateToCreateCommunity(context),
+                  ),
+            if (!isGuest)
+              ref.watch(userCommunitiesProvider).when(
+                data: (data) {
+                  return Expanded(
+                    child: ListView.builder(
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        final community = data[index];
+                        return ListTile(
+                          title: Text(community.name),
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(community.avatar),
+                          ),
+                          onTap: () {
+                            navigateToCommunity(context, community);
+                          },
+                        );
+                      },
                     ),
-                    onTap: () {
-                      navigateToCommunity(context, community);
-                    },
                   );
-                },),
-              );
-            }, error: (error, stackTrace) {
-              return Text(error.toString());
-            }, loading: () {
-              return const Loader();
-            },)
+                },
+                error: (error, stackTrace) {
+                  return Text(error.toString());
+                },
+                loading: () {
+                  return const Loader();
+                },
+              )
           ],
         ),
       ),
--- lib/features/post/screen/comment_screen.dart
+++ lib/features/post/screen/comment_screen.dart
@@ -7,6 +7,7 @@ import 'package:reddit_tutorial/features/post/controller/post_controller.dart';
 import 'package:reddit_tutorial/features/post/screen/widget/comment_card.dart';
 
 import '../../../models/post_model.dart';
+import '../../auth/controller/auth_controller.dart';
 
 class CommentScreen extends ConsumerStatefulWidget {
   CommentScreen({super.key, required this.postId});
@@ -36,6 +37,8 @@ class _CommentScreenState extends ConsumerState<CommentScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final user=ref.watch(userProvider)!;
+    final isGuest=!user.isAuthenticated;
     return Scaffold(
       appBar: AppBar(),
       body: ref.watch(getPostBiIdProvider(widget.postId)).when(
@@ -46,6 +49,7 @@ class _CommentScreenState extends ConsumerState<CommentScreen> {
               const SizedBox(
                 height: 10,
               ),
+              if(!isGuest)
               TextField(
                 onSubmitted: (value) => addComment(post: data),
                 controller: commentController,
